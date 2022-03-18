32ba01142c1fb82bf90906502c9593f5f865ba36
Rioting Pacifist <linux@riotingpacifist.net>
2022-03-18 00:27:15 -0700

    fix fill missing podcast mgiration

--- mygpo/subscriptions/migrations/0005_fill_missing_podcats.py
+++ mygpo/subscriptions/migrations/0005_fill_missing_podcats.py
@@ -1,10 +1,9 @@
 from django.db import migrations
 from mygpo.data.tasks import update_podcasts
 
-from mygpo.subscriptions.models import Subscription
-
 
 def forwards_func(apps, schema_editor):
+    Subscription = apps.get_model('subscriptions','Subscription')
     podcasts_urls = [
         sub.ref_url for sub in Subscription.objects.filter(podcast__link=None)
     ]
@@ -19,7 +18,7 @@ class Migration(migrations.Migration):
     atomic = False
 
     dependencies = [
-        ("podcasts", "0045_auto_20191230_2330"),
+        ("subscriptions", "0004_subscription_index"),
     ]
 
     operations = [migrations.RunPython(forwards_func, reverse_func)]
